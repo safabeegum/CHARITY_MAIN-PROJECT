6df8a91b8699e3b7093c0c3308dc9ed0c3b62b2b
safabeegum <pasafabeegum@gmail.com>
2025-02-19 14:28:00 +0530

    Register API Completed

--- charity-backend/app.js
+++ charity-backend/app.js
@@ -1,8 +1,38 @@
-const Express = rewuire("express")
+const Express = require("express")
 const Mongoose = require("mongoose")
+const Cors = require("cors")
 const Bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const Cors = require("cors")
- 
+const userModel = require("./models/users")
+
 let app = Express()
 
+app.use(Express.json())
+app.use(Cors())
+
+Mongoose.connect("mongodb+srv://safabeegum:mongodb24@cluster0.pbzbbey.mongodb.net/CharityApp?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/register", async(req,res)=> {
+    let input = req.body 
+    //Hashed password
+    let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hashedPassword
+
+
+    let check = await userModel.find({email:req.body.email})
+        if(check.length>0)
+        {
+            res.json({"status":"Email ID already exists"})
+        }
+        else
+        {
+            let result = new userModel(input)
+            await result.save()
+            res.json({"status":"Success"})
+        }
+
+})
+
+app.listen(3030, () =>{
+    console.log("Server Started")
+})
