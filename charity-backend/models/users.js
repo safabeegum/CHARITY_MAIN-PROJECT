d260e1358576d6286c052fed4aa90daac738e38e
safabeegum <pasafabeegum@gmail.com>
2025-02-24 22:53:53 +0530

    Admin Login API Completed

--- charity-backend/models/users.js
+++ charity-backend/models/users.js
@@ -33,6 +33,11 @@ const userSchema = Mongoose.Schema(
             required:true,
             enum:['1','2','3','4','5','6','7','8','9','10']
         },
+        ward_name:
+        {
+            type:String,
+            required:true
+        },
         password:
         {
             type:String,
